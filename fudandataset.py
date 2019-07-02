825ed3f43e88ce4de57d221fe094506bd0f05d4d
oolka1 <34199657+oolka1@users.noreply.github.com>
2019-07-02 21:45:41 +0100

    Update fudandataset.py

--- fudandataset.py
+++ fudandataset.py
@@ -137,10 +137,10 @@ class fudandataset(data.Dataset):
                         labels[labels==200]=1
                         labels[labels==500]=2
                         labels[labels==600]=3
-                        '''x=labels.shape[0]
+                        x=labels.shape[0]
                         x=int(0.31*x)
-                        labels=labels[x:x+192,]
-                        labels=labels[:,x:x+192]'''
+                        labels=labels[x:x+96,]
+                        labels=labels[:,x:x+96]
                         self.test_labels.append(labels)
                 
                         
@@ -152,14 +152,14 @@ class fudandataset(data.Dataset):
                     x= file_data1.shape[1]
                     for i in range(2,d):
                         data = copy.deepcopy(file_data1[:,:,i])
-                        '''x=data.shape[0]
+                        x=data.shape[0]
                         x=int(0.31*x)
-                        data=data[x:x+192,]
-                        data=data[:,x:x+192]
+                        data=data[x:x+96,]
+                        data=data[:,x:x+96]
                         data=data.astype(np.float32)
                         max1=data.max()
                         max1=max1.astype(np.float32)
-                        data=data/max1'''
+                        data=data/max1
                         self.test_data.append(data[:,:,np.newaxis].transpose(2,0,1)) #.transpose(2,0,1)
                         
          
